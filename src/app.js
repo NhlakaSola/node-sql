d2a913f031d5b14999d2d763f72b76df4f8ddcb4
Mzito <you@example.com>
2020-02-10 09:32:03 +0200

    added .env file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "user",
-  host: "localhost",
-  database: "db",
-  password: "pass",
-  port: 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port:process.env.DB_PORT,
+  database: process.env.DB_DATABASE
 });
 
 const addNewVisitor = (vName,vAge,dateOfVisit,timeOfVisit,assistantName,comments) => {
